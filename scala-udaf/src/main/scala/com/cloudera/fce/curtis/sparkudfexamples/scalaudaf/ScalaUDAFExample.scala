6b9223b67eebffbd38c0c045668dd958cb9cfb63
Curtis Howard <curtis.james.howard@gmail.com>
2017-01-01 18:57:12 -0500

    Update ScalaUDAFExample.scala

--- scala-udaf/src/main/scala/com/cloudera/fce/curtis/sparkudfexamples/scalaudaf/ScalaUDAFExample.scala
+++ scala-udaf/src/main/scala/com/cloudera/fce/curtis/sparkudfexamples/scalaudaf/ScalaUDAFExample.scala
@@ -9,30 +9,31 @@ import org.apache.spark.sql.SQLContext
 
 object ScalaUDAFExample {
 
+  // Define the SparkSQL UDAF logic
   private class SumProductAggregateFunction extends UserDefinedAggregateFunction {
-
-    def inputSchema: StructType =
+    // Define the UDAF input and result schema's
+    def inputSchema: StructType =     // Input  = (Double price, Long quantity)
       new StructType().add("price", DoubleType).add("quantity", LongType)
-    def bufferSchema: StructType =
-      new StructType().add("sumPrices", DoubleType)
+    def bufferSchema: StructType =    // Output = (Double total)
+      new StructType().add("total", DoubleType)
     def dataType: DataType = DoubleType
-    def deterministic: Boolean = true
+    def deterministic: Boolean = true // true: our UDAF's output given an input is deterministic
 
     def initialize(buffer: MutableAggregationBuffer): Unit = {
-      buffer.update(0, 0.0)
+      buffer.update(0, 0.0)           // Initialize the result to 0.0
     }
 
     def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-      val sum   = buffer.getDouble(0)
-      val price = input.getDouble(0)
-      val qty   = input.getLong(1)
-      buffer.update(0, sum + (price * qty))
+      val sum   = buffer.getDouble(0) // Intermediate result to be updated
+      val price = input.getDouble(0)  // First input parameter
+      val qty   = input.getLong(1)    // Second input parameter
+      buffer.update(0, sum + (price * qty))   // Update the intermediate result
     }
-
+    // Merge intermediate result sums by adding them
     def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
       buffer1.update(0, buffer1.getDouble(0) + buffer2.getDouble(0))
     }
-
+    // THe final result will be contained in 'buffer'
     def evaluate(buffer: Row): Any = {
       buffer.getDouble(0)
     }
@@ -46,7 +47,7 @@ object ScalaUDAFExample {
 
     val testDF = sqlContext.read.json("inventory.json")
     testDF.registerTempTable("inventory") 
-
+    // Register the UDAF with our SQLContext
     sqlContext.udf.register("SUMPRODUCT", new SumProductAggregateFunction)
 
     sqlContext.sql("SELECT Make, SUMPRODUCT(RetailValue,Stock) as InventoryValuePerMake FROM inventory GROUP BY Make").show()
