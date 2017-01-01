076335727a5c029879c2a63e5d60cbd2edb9dccc
Curtis Howard <curtis.james.howard@gmail.com>
2017-01-01 15:59:47 -0500

    Update python-udf-example.py

--- python-udf/python-udf-example.py
+++ python-udf/python-udf-example.py
@@ -8,6 +8,7 @@ sqlContext = SQLContext(sc)
 df         = sqlContext.read.json("temperatures.json")
 df.registerTempTable("citytemps")
 
+# Register the UDF with our SQLContext
 sqlContext.registerFunction("CTOF", lambda celcius: ((celcius * 9.0 / 5.0) + 32.0))
 
 sqlContext.sql("SELECT city, CTOF(avgLow) AS avgLow, CTOF(avgHigh) AS avgHigh FROM citytemps").show()
