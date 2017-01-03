4eec80d0e5237b77d0293c6ae2159000ac384e1a
Hadoop HDFS <hdfs@quickstart.cloudera>
2017-01-03 08:41:56 -0800

    .

--- hive-udf/src/main/java/com/cloudera/fce/curtis/sparkudfexamples/hiveudf/CtoF.java
+++ hive-udf/src/main/java/com/cloudera/fce/curtis/sparkudfexamples/hiveudf/CtoF.java
@@ -1,10 +1,7 @@
 package com.cloudera.fce.curtis.sparkudfexamples.hiveudf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
-import org.apache.hadoop.io.DoubleWritable;
-
-//import java.text.NumberFormat;
-//import java.util.Locale;
+//import org.apache.hadoop.io.DoubleWritable;
 
 public class CtoF extends UDF {
   public Double evaluate(Double celcius) {
