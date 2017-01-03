daad88f6cc8613c68a685cab6e1ee2b2b40e9006
Hadoop HDFS <hdfs@quickstart.cloudera>
2017-01-03 07:36:44 -0800

    .

---
+++ scala-from-python-udaf/scala-from-python-udaf.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+
+conf = SparkConf().setAppName("Scala UDAF from Python example")
+sc   = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+
+df = sqlContext.read.json("inventory.json")
+df.registerTempTable("inventory")
+
+scala_sql_context  =  sqlContext._ssql_ctx
+scala_spark_context = sqlContext._sc
+scala_spark_context._jvm.com.cloudera.fce.curtis.sparkudfexamples.scalafrompythonudaf.ScalaUDAFForPythonExample.registerUdfs(scala_sql_context)
+
+sqlContext.sql("SELECT Make, SUMPRODUCT(RetailValue,Stock) as InventoryValuePerMake FROM inventory GROUP BY Make").show()
---
+++ scala-from-python-udaf/src/main/scala/com/cloudera/fce/curtis/sparkudfexamples/scalafrompythonudf/ScalaUDFForPythonExample.scala
@@ -0,0 +1,44 @@
+package com.cloudera.fce.curtis.sparkudfexamples.scalafrompythonudaf
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.SQLContext
+
+object ScalaUDAFForPythonExample {
+
+  private class SumProductAggregateFunction extends UserDefinedAggregateFunction {
+    def inputSchema: StructType =     
+      new StructType().add("price", DoubleType).add("quantity", LongType)
+    def bufferSchema: StructType =    
+      new StructType().add("total", DoubleType)
+    def dataType: DataType = DoubleType
+    def deterministic: Boolean = true 
+
+    def initialize(buffer: MutableAggregationBuffer): Unit = {
+      buffer.update(0, 0.0)      
+    }
+
+    def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+      val sum   = buffer.getDouble(0) 
+      val price = input.getDouble(0) 
+      val qty   = input.getLong(1)   
+      buffer.update(0, sum + (price * qty))  
+    }
+   
+    def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+      buffer1.update(0, buffer1.getDouble(0) + buffer2.getDouble(0))
+    }
+
+    def evaluate(buffer: Row): Any = {
+      buffer.getDouble(0)
+    }
+  }
+
+  // THIS FUNCTION IS CALLED FROM PySpark TO REGISTER OUR UDAF
+  def registerUdfs(sqlCtx: SQLContext) {
+    sqlCtx.udf.register("SUMPRODUCT", new SumProductAggregateFunction)
+  }
+}
