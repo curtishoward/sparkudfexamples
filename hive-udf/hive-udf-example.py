901fca36938bb5c7778a5061317e14f486fedd8f
Hadoop HDFS <hdfs@quickstart.cloudera>
2017-01-04 20:39:46 -0800

    .

--- hive-udf/hive-udf-example.py
+++ hive-udf/hive-udf-example.py
@@ -9,6 +9,6 @@ df         = sqlContext.read.json("temperatures.json")
 df.registerTempTable("citytemps")
 
 # Register our Hive UDF
-sqlContext.sql("CREATE TEMPORARY FUNCTION CtoF AS 'com.cloudera.fce.curtis.sparkudfexamples.hiveudf.CtoF'")
+sqlContext.sql("CREATE TEMPORARY FUNCTION CTOF AS 'com.cloudera.fce.curtis.sparkudfexamples.hiveudf.CTOF'")
 
-sqlContext.sql("SELECT city, CTOF(avgLow) AS avgLow, CTOF(avgHigh) AS avgHigh FROM citytemps").show()
+sqlContext.sql("SELECT city, CTOF(avgLow) AS avgLowF, CTOF(avgHigh) AS avgHighF FROM citytemps").show()
--- hive-udf/src/main/java/com/cloudera/fce/curtis/sparkudfexamples/hiveudf/CTOF.java
+++ hive-udf/src/main/java/com/cloudera/fce/curtis/sparkudfexamples/hiveudf/CTOF.java
@@ -3,8 +3,8 @@ package com.cloudera.fce.curtis.sparkudfexamples.hiveudf;
 import org.apache.hadoop.hive.ql.exec.UDF;
 //import org.apache.hadoop.io.DoubleWritable;
 
-public class CtoF extends UDF {
-  public Double evaluate(Double celcius) {
-    return ((celcius * 9.0 / 5.0) + 32.0);
+public class CTOF extends UDF {
+  public Double evaluate(Double degreesCelsius) {
+    return ((degreesCelsius * 9.0 / 5.0) + 32.0);
   }
 }
